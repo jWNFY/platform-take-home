043e824d2d784be937911a20c6466dcdd28845f8
jWNFY <jWNFY@pm.me>
2024-12-02 23:08:40 +0100

    chore: optimized proto-gen.sh removing complexity

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -1,20 +1,11 @@
+#!/bin/bash
 echo "Generating proto code"
 cd proto
-proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
-      buf generate --template buf.gen.yaml $file
-    fi
-  done
-done
 
-# Generate external protocol buffers
-# echo "Generating cosmwasm protos"
-# buf generate buf.build/cosmwasm/wasmd
+# Ensure we have the latest dependencies
+buf dep update
 
-rm -r ../api/types
-mv github.com/skip-mev/platform-take-home/api/types ../api
-rm -r github.com
+# Generate the code
+buf generate
 
-# move proto files to the right places
+cd ..
